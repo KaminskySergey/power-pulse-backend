cbf9118b956c4ffdab94bf0ffe638b1da661203f
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2024-10-22 02:07:28 +0300

    origin change

--- dist/main.js
+++ dist/main.js
@@ -11,7 +11,7 @@ async function bootstrap() {
     app.use(bodyParser.json({ limit: '10mb' }));
     app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
     app.enableCors({
-        origin: ['http://localhost:3000'],
+        origin: ['https://power-pulse-backend-uy3b.onrender.com'],
         credentials: true,
         exposedHeaders: 'set-cookie',
     });
