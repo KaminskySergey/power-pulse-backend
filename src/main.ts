bfc6c8878a7c2e3fa4c8674910706d9facb29270
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2024-10-23 14:00:28 +0300

    add new origin

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
   app.enableCors({
-    origin: ['https://power-pulse-backend-uy3b.onrender.com'],
+    origin: ['https://power-pulse-six.vercel.app'],
     credentials: true,
     exposedHeaders: 'set-cookie',
   });
